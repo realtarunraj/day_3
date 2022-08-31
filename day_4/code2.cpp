8ed19eed27580a474bf55c40d91dc3f8dbe76db3
realtarunraj <realtarunraj@gmail.com>
2022-08-31 12:27:23 +0530

    next Code

---
+++ day_4/code2.cpp
@@ -0,0 +1,22 @@
+    // 97531
+    // 7531
+    // 531
+    // 31
+    // 1
+
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    int i,j;
+
+    cin<<n;
+
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+}
